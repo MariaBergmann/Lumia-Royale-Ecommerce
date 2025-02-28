99727b4cea43f289a49934f3c1a65f5d80fb7619
MariaBergmann <162642619+MariaBergmann@users.noreply.github.com>
2025-02-28 16:57:20 -0300

    Removendo o django-debug-toolbar

--- loja/urls.py
+++ loja/urls.py
@@ -28,9 +28,3 @@ urlpatterns = [
     path('pedido/', include('pedidos.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('debug/', include(debug_toolbar.urls)),
-    ] + urlpatterns
